ca68736f6097c898eda48b06db0b298b613447c0
anilbhangay <anilbhangay@gmail.com>
2024-07-22 18:40:08 +0530

    Created new files for Advain application

---
+++ src/Components/Advocate-Register/Login.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import bgimg from "../../Assets/Backgrdimage.png";
+import logov1 from "../../Assets/LogoV1.png";
+import "./Login.css";
+
+const Login = () => {
+  return (
+    <div className="container">
+      <div id="logo-1">
+        <img src={logov1} alt="No" />
+      </div>
+      <div id="bgcimg-1">
+        <img src={bgimg} alt="No" />
+      </div>
+       <form>
+          
+       </form>
+    </div>
+  );
+};
+
+export default Login;
