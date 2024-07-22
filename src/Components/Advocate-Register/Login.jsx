7d5acdef12a87276bd724c9a3d66cb05e65a2914
anilbhangay <anilbhangay@gmail.com>
2024-07-22 20:02:22 +0530

    Added form in login page

--- src/Components/Advocate-Register/Login.jsx
+++ src/Components/Advocate-Register/Login.jsx
@@ -12,11 +12,29 @@ const Login = () => {
       <div id="bgcimg-1">
         <img src={bgimg} alt="No" />
       </div>
-<<<<<<< HEAD
-      
-=======
-      
->>>>>>> be8a9ab33f30553055375866c011a6c21e245e58
+        <div className="login-form-container">
+          <form>
+            <div className="form-header">
+            <h3>LOGIN</h3>
+             <h3>REGISTER</h3>
+            </div>
+            <div className="form-input">
+              <p>User ID</p>
+              <input type="email" name="email" placeholder="Enter 10-digit Mobile no.*" />
+            </div>
+            <div className="form-input">
+              <p>Password</p>
+              <input type="password" name="password" placeholder="Enter Your Password*" />
+            </div>
+             <div className="form-links-login">
+              <h4><a id="forgot-pass" href="/">Forgot Password ?</a></h4>
+              <button type="submit" id="login-button">Login</button>
+              <p id="space">---- OR ----</p>
+              <button type="submit" id="otp-button">Login with OTP</button>
+                <h4 id="advo-name">ADVOCATE LOGIN</h4>
+             </div>
+          </form>
+        </div>
     </div>
   );
 };
