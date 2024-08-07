67fbd0181644a239bc39708b7c4da1011e284a6c
anilbhangay <anilbhangay@gmail.com>
2024-08-07 14:29:04 +0530

    Added style to successpassword

--- src/Components/Advocate-Login/Successpassword.jsx
+++ src/Components/Advocate-Login/Successpassword.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import './Successpassword';
+import './Successpassword.css';
 
 
 const  Successpassword = () => {
